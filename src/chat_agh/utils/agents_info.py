b6dee34338a8a81f4ebbdd58c0b7a45fe349415d
witoldnowogorski <witek.nowogorski@ibm.com>
2025-10-22 21:24:18 +0200

    update

--- src/chat_agh/utils/agents_info.py
+++ src/chat_agh/utils/agents_info.py
@@ -45,33 +45,83 @@ RETRIEVAL_AGENTS = [
         przekierowania do innych domen/informacji. Zawiera również informacje z Centurm Obsługi Kształcenia
         gdzie można znaleźć informacje dotyczące organizacji i koordynacji procesu kształcenia na studiach wyższych i podyplomowych,
         w tym obsługi systemu USOS, zasad i procedur dydaktycznych oraz wsparcia technicznego dla jednostek uczelni.
-        
-        Agent obsługuje następujące domeny:
+
+        Obsługuje domeny:
         - agh.edu.pl
         - cok.agh.edu.pl
         """,
     ),
     RetrievalAgentInfo(
-        name="wh_agent",
+        name="Wydział Humanistyczny agent",
         vector_store_index_name="cluster_1",
         description="""
         Agent Wydziału Humanistycznego AGH
-        
-        Agent obsługuje następujące domeny:
+
+        Obsługuje domeny:
         - wh.agh.edu.pl
         - power.3.5.wh.agh.edu.pl
         - konfpau.wh.agh.edu.pl
         - multisite.wh.agh.edu.pl
         """,
     ),
-    # RetrievalAgentInfo(
-    #     name="dss_agent",
-    #     vector_store_index_name="dss",
-    #     description="""
-    #     Agent wyszukujący informacji z działu spraw studenckich AGH.
-    #     Informacje o procedurach administracyjnych, takich jak składanie wniosków o stypendia,
-    #      urlopy dziekańskie czy wydawanie zaświadczeń.
-    #       Dodatkowo często dostępne są wzory dokumentów, regulaminy studiów i informacje dotyczące akademików, pomocy materialnej czy szeroko pojętej działąlności uczelni.
-    #     """,
-    # ),
+    RetrievalAgentInfo(
+        name="Wydział Informatyczny agent",
+        vector_store_index_name="cluster_2",
+        description="""
+        Agent Wydziału Informatyki AGH
+
+        Obsługuje domeny:
+        - informatyka.agh.edu.pl
+        """,
+    ),
+    RetrievalAgentInfo(
+        name="Podyplomoce agent",
+        vector_store_index_name="cluster_3",
+        description="""
+        Agent zajmujący sie studiami podyplomowymi agh oraz studenckimi kołami naukowymi na agh.
+
+        Obsługuje domeny:
+        - informatyka.podyplomowe.agh.edu.pl
+        - podyplomowe.agh.edu.pl
+        - skn.agh.edu.pl
+        """,
+    ),
+    RetrievalAgentInfo(
+        name="Wydział Inżynierii Lądowej i Gospodarki Zasobami agent",
+        vector_store_index_name="cluster_4",
+        description="""
+        Agent Wydział Inżynierii Lądowej i Gospodarki Zasobami
+
+        Obsługuje domeny:
+        - wilgz.agh.edu.pl
+        """,
+    ),
+    RetrievalAgentInfo(
+        name="Oferta badawcza agh agent",
+        vector_store_index_name="cluster_5",
+        description="""
+        Infmroacje o ofercie badawczej agh, bazie aparatury, zespołach badawczych itd.
+
+        Obsługuje domeny:
+        - oferta-badawcza.agh.edu.pl
+        """,
+    ),
+    RetrievalAgentInfo(
+        name="SKOS AGH",
+        vector_store_index_name="cluster_6",
+        description="""
+        System Informacyjny AGH, informacje o pracownikach, ciałach kolegialnych i jednostkach organizacyjnych.
+
+        Obsługuje domeny:
+        - skos.agh.edu.pl
+        - old.skos.agh.edu.pl
+        """,
+    ),
+    RetrievalAgentInfo(
+        name="Sprawy studenckie",
+        vector_store_index_name="cluster_7",
+        description="""
+
+        """,
+    ),
 ]
