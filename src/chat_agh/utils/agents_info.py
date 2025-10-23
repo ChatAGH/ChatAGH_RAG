8e91eacfbfb3dda1e1ca04145114507829f0a99f
witoldnowogorski <witek.nowogorski@ibm.com>
2025-10-23 11:12:52 +0200

    retrieval agents descriptions

--- src/chat_agh/utils/agents_info.py
+++ src/chat_agh/utils/agents_info.py
@@ -38,13 +38,17 @@ class RetrievalAgentInfo:
 
 RETRIEVAL_AGENTS = [
     RetrievalAgentInfo(
-        name="main_agent",
+        name="MAIN AGENT",
         vector_store_index_name="cluster_0",
         description="""
-        Agent pobierający informację z głównej strony agh, na której znajdują się podstawowe informacje o uczelni i
-        przekierowania do innych domen/informacji. Zawiera również informacje z Centurm Obsługi Kształcenia
-        gdzie można znaleźć informacje dotyczące organizacji i koordynacji procesu kształcenia na studiach wyższych i podyplomowych,
-        w tym obsługi systemu USOS, zasad i procedur dydaktycznych oraz wsparcia technicznego dla jednostek uczelni.
+        Informacje ogólne i obsługa dydaktyki – Akademia Górniczo‑Hutnicza im. Stanisława Staszica w Krakowie
+
+        Zakres treści:
+	    -	Strona główna uczelni, ogólne informacje o uczelni – misja, historia, wydziały, jednostki.  ￼
+	    -	Informacje dla kandydatów, studentów i doktorantów – oferta studiów, kierunki, procedury, zasoby dydaktyczne.  ￼
+	    -	Jednostka zajmująca się obsługą kształcenia – Centrum Obsługi Kształcenia (COK) – regulaminy, systemy, wsparcie studenta.  ￼
+	    -	Zewnętrzne i wewnętrzne serwisy uczelni: platformy, e-usługi, ogłoszenia, dane kontaktowe.  ￼
+	    -	Informacje administracyjne uczelni – struktura, jednostki, dane o przetwarzaniu danych.
 
         Obsługuje domeny:
         - agh.edu.pl
@@ -52,76 +56,72 @@ RETRIEVAL_AGENTS = [
         """,
     ),
     RetrievalAgentInfo(
-        name="Wydział Humanistyczny agent",
-        vector_store_index_name="cluster_1",
-        description="""
-        Agent Wydziału Humanistycznego AGH
-
-        Obsługuje domeny:
-        - wh.agh.edu.pl
-        - power.3.5.wh.agh.edu.pl
-        - konfpau.wh.agh.edu.pl
-        - multisite.wh.agh.edu.pl
-        """,
-    ),
-    RetrievalAgentInfo(
-        name="Wydział Informatyczny agent",
-        vector_store_index_name="cluster_2",
-        description="""
-        Agent Wydziału Informatyki AGH
-
-        Obsługuje domeny:
-        - informatyka.agh.edu.pl
-        """,
-    ),
-    RetrievalAgentInfo(
-        name="Podyplomoce agent",
-        vector_store_index_name="cluster_3",
-        description="""
-        Agent zajmujący sie studiami podyplomowymi agh oraz studenckimi kołami naukowymi na agh.
-
-        Obsługuje domeny:
-        - informatyka.podyplomowe.agh.edu.pl
-        - podyplomowe.agh.edu.pl
-        - skn.agh.edu.pl
-        """,
-    ),
-    RetrievalAgentInfo(
-        name="Wydział Inżynierii Lądowej i Gospodarki Zasobami agent",
-        vector_store_index_name="cluster_4",
-        description="""
-        Agent Wydział Inżynierii Lądowej i Gospodarki Zasobami
-
-        Obsługuje domeny:
-        - wilgz.agh.edu.pl
-        """,
-    ),
-    RetrievalAgentInfo(
-        name="Oferta badawcza agh agent",
-        vector_store_index_name="cluster_5",
-        description="""
-        Infmroacje o ofercie badawczej agh, bazie aparatury, zespołach badawczych itd.
-
-        Obsługuje domeny:
-        - oferta-badawcza.agh.edu.pl
-        """,
-    ),
-    RetrievalAgentInfo(
-        name="SKOS AGH",
+        name="SKOS AGENT",
         vector_store_index_name="cluster_6",
         description="""
-        System Informacyjny AGH, informacje o pracownikach, ciałach kolegialnych i jednostkach organizacyjnych.
+        System informacji o pracownikach & jednostkach – SkOs AGH
 
-        Obsługuje domeny:
-        - skos.agh.edu.pl
+        Zakres treści (scope):
+	    -	Baza informacji kontaktowych pracowników, ich afiliacji, jednostek, stanowisk w AGH.  ￼
+	    -	Informacje o strukturze organizacyjnej uczelni: jednostki, wydziały, instytuty, ciała kolegialne, etc.  ￼
+	    -	Funkcje wyszukiwania (podstawowe, zaawansowane) w systemie SkOs, np. wyszukiwanie pracownika po nazwisku, jednostce.  ￼
+
+	    Obsługuje domeny:
+	    - skos.agh.edu.pl
         - old.skos.agh.edu.pl
         """,
     ),
     RetrievalAgentInfo(
-        name="Sprawy studenckie",
+        name="SPRAWY STUDENCKIE AGENT",
         vector_store_index_name="cluster_7",
         description="""
+        Zakwaterowanie & sprawy studenckie – kampus i wsparcie studenta Akademia Górniczo‑Hutnicza im. Stanisława Staszica (AGH)”
 
+        Zakres treści (scope):
+	    -	Kompleks akademików i osiedla studenckiego: Miasteczko Studenckie AGH – informacje o domach studenckich, zakwaterowaniu, cennikach, standardach pokoi.  ￼
+	    -	Procesy i regulaminy przydziału miejsc w domach studenckich: wnioski, harmonogramy, rejestracja.  ￼
+	    -	Obsługa spraw studenckich: prawa i obowiązki studentów, świadczenia, pomoc, regulacje uczelni.  ￼
+
+	    Obsługuje domeny:
+	    - miasteczko.agh.edu.pl
+        - akademik.agh.edu.pl
+        - rownosc.agh.edu.pl
+        - dss.agh.edu.pl
+        """,
+    ),
+    RetrievalAgentInfo(
+        name="REKRUTACJA AGENT",
+        vector_store_index_name="cluster_8",
+        description="""
+        Nazwa klastra: „Rekrutacja, szkolenia i promocja – usługi kształcenia i wizerunku Akademia Górniczo‑Hutnicza im Stanisława Staszica (AGH)”
+
+        Zakres treści (scope):
+        - Proces rekrutacji kandydatów na studia – portal rekrutacja.agh.edu.pl: terminy, warunki, procedury, skierowany do kandydatów.
+        - Oferta kursów i szkoleń (zarówno wewnętrznych dla pracowników/studentów, jak i zewnętrznych) – serwis „Szkolenia” AGH.  ￼
+        - Program strategiczny „Inicjatywa Doskonałości Uczelnia Badawcza (IDUB)” – działania badawcze, inwestycje AGH w badania, rozwój kadry i infrastruktury.  ￼￼
+        - Strona marki, wizerunku i komunikacji uczelni – Centrum Komunikacji i Marketingu AGH (CKiM) odpowiadająca za portal uczelni i działania promocyjne.  ￼
+
+        Obsługuje domeny:
+        - rekrutacja.agh.edu.pl
+        - ckim.agh.edu.pl
+        - szkolenia.agh.edu.pl
+        - idub.agh.edu.pl
+        """,
+    ),
+    RetrievalAgentInfo(
+        name="SYLABUSY AGENT",
+        vector_store_index_name="cluster_9",
+        description="""
+        Nazwa klastra: „Zamówienia publiczne & sylabusy-oferta dydaktyczna – Akademia Górniczo‑Hutnicza im. Stanisława Staszica (AGH)”
+
+        Zakres treści (scope):
+        - Platforma Sylabusy AGH (sylabusy.agh.edu.pl) – katalog kierunków, programy studiów, moduły, efekty kształcenia, sylabusy przedmiotów – zarówno w języku polskim jak i angielskim.  ￼
+        - Serwis Dział Zamówień Publicznych AGH (dzp.agh.edu.pl) – ogłoszenia przetargowe, plany zamówień uczelni, informacje formalne związane z zamówieniami publicznymi w AGH.  ￼
+        - Zdaniem tej domeny: dokumenty dydaktyczne (opis przedmiotów, programy, sylabusy) oraz dokumentacja formalna uczelni związana z zamówieniami (dla AGH jako instytucji publicznej)
+
+        Obsługuje domeny:
+        - dzp.agh.edu.pl
+        - sylabusy.agh.edu.pl
         """,
     ),
 ]
